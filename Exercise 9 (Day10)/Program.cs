adf992863890fdc96b8ff41ed2440238b9e3681c
HP <HP@VLADIKPROGRAMME>
2023-03-06 21:11:50 +0300

    Добавьте файлы проекта.

---
+++ Exercise 9 (Day10)/Program.cs
@@ -0,0 +1,93 @@
+﻿// Объявляем интерфейсы Ix, Iy, Iz
+interface Ix
+{
+    void IxF0(double параметр);
+    void IxF1();
+}
+
+interface Iy
+{
+    void F0(double параметр);
+    void F1();
+}
+
+interface Iz
+{
+    void F0(double параметр);
+    void F1();
+}
+
+// Класс TestClass, который наследует интерфейсы Ix, Iy, Iz и реализует их методы
+class TestClass : Ix, Iy, Iz
+{
+    // Член w класса TestClass
+    public double w;
+
+// Неявная реализация методов интерфейсов Iy и Iz
+public void F0(double x)
+    {
+        Console.WriteLine("Результат неявной реализации метода F0: " + Math.Sqrt(x));
+    }
+
+    public void F1()
+    {
+        Console.WriteLine("Результат неявной реализации метода F1");
+    }
+
+    public void IxF0(double x)
+    {
+        Console.WriteLine("Результат неявной реализации метода IxF0: " + Math.Pow(x, 2));
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine("Результат неявной реализации метода IxF1");
+    }
+
+    // Явная реализация метода интерфейса Iz
+    void Iz.F0(double z)
+    {
+        Console.WriteLine("Результат явной реализации метода F0: " + ((z * z) + 5));
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine("Результат явной реализации метода F1");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем объект класса TestClass
+        TestClass obj = new TestClass();
+
+        // Присваиваем значение члену w
+    obj.w = 4.0;
+
+        // Вызываем методы объекта через интерфейсы
+        Ix objIx = obj;
+        objIx.IxF0(obj.w);
+        objIx.IxF1();
+
+        Iy objIy = obj;
+        objIy.F0(obj.w);
+        objIy.F1();
+
+        Iz objIz = obj;
+        objIz.F0(obj.w);
+        objIz.F1();
+
+        // Вызываем метод объекта через интерфейсную ссылку с параметром типа double
+        IxF0((double)obj.w);
+    }
+
+    // Метод, который вызывает метод IxF0 объекта через интерфейсную ссылку с параметром типа double
+    static void IxF0(double параметр)
+    {
+        TestClass obj = new TestClass();
+        Ix objIx = obj;
+        objIx.IxF0(параметр);
+    }
+}
---
+++ z2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Massiv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Massiv mas1 = new Massiv(n);
+            mas1.ShowAll();
+
+            Console.Write("Введите индекс элемента массива i = ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            mas1.ShowElement(i);
+
+            Massiv mas2 = new Massiv(n);
+            mas2.ShowAll();
+
+            Massiv mas3 = mas1 + mas2;
+            mas3.ShowAll();
+
+            Massiv mas4 = mas1 - mas2;
+            mas4.ShowAll();
+
+            Console.Write("Введите целое число num = ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            mas3 = mas3 * num;
+            mas3.ShowAll();
+
+            mas3 = mas3 / num;
+            mas3.ShowAll();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Massiv
+    {
+        public Massiv(int n)
+        {
+            mas = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                mas[i] = rand.Next(0, 10);
+            }
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine("Массив: ");
+            foreach (var elem in mas)
+            {
+                Console.Write(elem + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public string ShowElement(int index)
+        {
+            try
+            {
+                return "Элемент массива с индексом " + index + " mas[" + index + "] = " + mas[index];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return "Попытка обращения к элементу массива с индексом, который находится вне границ массива.";
+
+            }
+        }
+
+        public static Massiv operator +(Massiv mas1, Massiv mas2)
+        {
+
+            if (mas1.Length == mas2.Length)
+            {
+                Massiv mas = new Massiv(mas1.Length);
+                for (int i = 0; i < mas1.Length; ++i)
+                {
+                    mas[i] = mas1[i] + mas2[i];
+                }
+                return mas;
+            }
+
+            Console.WriteLine("Значения индексов массивов не совпадают.");
+            return null;
+        }
+
+        public static Massiv operator -(Massiv mas1, Massiv mas2)
+        {
+
+            if (mas1.Length == mas2.Length)
+            {
+                Massiv mas = new Massiv(mas1.Length);
+                for (int i = 0; i < mas1.Length; ++i)
+                {
+                    mas[i] = mas1[i] - mas2[i];
+                }
+                return mas;
+            }
+
+            Console.WriteLine("Значения индексов массивов не совпадают.");
+            return null;
+        }
+
+        public static Massiv operator *(Massiv mas1, int num)
+        {
+            Massiv mas = new Massiv(mas1.Length);
+            for (int i = 0; i < mas1.Length; ++i)
+            {
+                mas[i] = mas1[i] * num;
+            }
+            return mas;
+        }
+
+        public static Massiv operator /(Massiv mas1, int num)
+        {
+            Massiv mas = new Massiv(mas1.Length);
+            for (int i = 0; i < mas1.Length; ++i)
+            {
+                mas[i] = mas1[i] / num;
+            }
+            return mas;
+        }
+
+        public int Length
+        {
+            get { return mas.Length; }
+        }
+
+        public int this[int index]
+        {
+            get { return mas[index]; }
+            set { mas[index] = value; }
+        }
+
+        private int[] mas;
+    }
+}
